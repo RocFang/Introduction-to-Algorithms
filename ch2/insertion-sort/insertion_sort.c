5fa587ce5e64f76560bd5aeb54813bb9343d4864
fangpeng <fangpeng1986@gmail.com>
2014-06-24 20:01:14 +0800

    optimize the insertion_sort

--- ch2/insertion-sort/insertion_sort.c
+++ ch2/insertion-sort/insertion_sort.c
@@ -1,20 +1,15 @@
 void insertion_sort(int a[], int n)
 {
-    int i, j, k, tmp;
+    int i, j, tmp;
     for(i = 1; i < n; i++)
     {
         tmp = a[i]; 
-        for(j = 0; j < i; j++)
+        j = i-1;
+        while(a[j] > tmp && j >= 0)
         {
-            if(a[j] > a[i])
-            {
-                for(k = i; k > j; k--)
-                {
-                    a[k] = a[k-1];
-                }
-                a[j] = tmp;
-                break;
-            }
-        }         
+            a[j+1] = a[j];
+            j--;
+        }
+        a[j+1] = tmp;
     }  
 }
